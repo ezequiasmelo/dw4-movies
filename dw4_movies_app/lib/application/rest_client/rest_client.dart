b9e3614e07ec62643061a548994fcf2d0c9de85c
ezequiasmelo <ezequias-rm@hotmail.com>
2023-03-01 00:08:32 -0300

    adicionado a url da api do themoviedb no .env

--- dw4_movies_app/lib/application/rest_client/rest_client.dart
+++ dw4_movies_app/lib/application/rest_client/rest_client.dart
@@ -1,7 +1,9 @@
 import 'package:get/get_connect/connect.dart';
 
+import '../config/env/env.dart';
+
 class RestClient extends GetConnect {
   RestClient() {
-    httpClient.baseUrl = 'https://api.themoviedb.org/3';
+    httpClient.baseUrl = Env.i['api_url_themoviedb'] ?? '';
   }
 }
